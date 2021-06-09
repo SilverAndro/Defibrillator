f9c992059fbf4a7bbef13d2734787c447abac645
P03W <P03W@users.noreply.github.com>
2021-06-09 09:42:32 -0400

    Improve and optimise the regex matching

--- src/main/kotlin/mc/defibrillator/util/StringUtil.kt
+++ src/main/kotlin/mc/defibrillator/util/StringUtil.kt
@@ -9,7 +9,7 @@ package mc.defibrillator.util
 /**
  * Removes the postfix from numbers if it exists
  */
+private val regex = Regex("^([\\-+]?\\d+\\.?(?:\\d+)?)")
 fun String.cleanNumber(): String {
-    val regex = Regex("^([\\-+]?[\\d\\.]+)\\w?")
     return regex.find(this)?.groupValues?.get(1) ?: "0"
 }
